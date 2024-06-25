a6a3fc176c217ea179a03fd51b4b0d984b1e536b
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2024-06-25 18:53:00 -0400

    Added error handler route.

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 from werkzeug.exceptions import HTTPException
 
+@app.errorhandler(Exception)
+def handle_exception(e):
+    if isinstance(e, HTTPException):
+        print(e)
+        return e
+      
+    return render_template("500.html", error_message=e), 500
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
 
